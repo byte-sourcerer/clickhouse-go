98fc9378ecd29d2a1f7f60760bcce718a71de679
Kermit <chenjiawei1@xiaohongshu.com>
2024-12-23 11:45:42 +0800

    wip

--- lib/proto/buffer.go
+++ lib/proto/buffer.go
@@ -4,11 +4,11 @@ import chproto "github.com/ClickHouse/ch-go/proto"
 
 // 必须保证 Final Block 中 row number > 0
 type FinalBlock struct {
-	buffer *chproto.Buffer
-	query string
+	buffer       *chproto.Buffer
+	startIndices []int
+	query        string
 }
 
 func (b *FinalBlock) GetQuery() string {
 	return b.query
 }
-
