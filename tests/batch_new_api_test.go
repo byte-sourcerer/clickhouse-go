35c29d9b835b2a0c9ea3fea646b630392cc69342
Kermit <chenjiawei1@xiaohongshu.com>
2024-12-30 19:47:37 +0800

    add test

--- tests/batch_new_api_test.go
+++ tests/batch_new_api_test.go
@@ -1,18 +1,96 @@
 package tests
 
-// import (
-// 	"context"
-// 	"math/big"
-// 	"os"
-// 	"testing"
+import (
+	"context"
+	"math/rand"
+	"sync"
+	"testing"
 
-// 	"github.com/ClickHouse/clickhouse-go/v2"
-// 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	"github.com/ClickHouse/clickhouse-go/v2"
+	bf "github.com/ClickHouse/clickhouse-go/v2/lib/buffer"
 
-// 	bf "github.com/ClickHouse/clickhouse-go/v2/lib/buffer"
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/stretchr/testify/require"
-// )
+	"github.com/stretchr/testify/require"
+)
+
+func TestSplitSend(t *testing.T) {
+	// rand.New(rand.NewSource(23462))
+
+	conn, err := GetNativeConnection(
+		clickhouse.Settings{
+			"max_execution_time": 60,
+		},
+		nil,
+		&clickhouse.Compression{
+			Method: clickhouse.CompressionLZ4,
+		},
+	)
+	require.NoError(t, err)
+
+	conn.Exec(context.Background(), "DROP TABLE IF EXISTS example")
+	require.NoError(t, conn.Exec(
+		context.Background(),
+		`
+			CREATE TABLE example (
+				i64 Int64,
+				s String,
+				low_s LowCardinality(String),
+				msg String CODEC(ZSTD(1)),
+				msgOver String CODEC(ZSTD(5))
+			)
+			ENGINE = Memory
+			`,
+	))
+
+	buildRandomRow := func() []any {
+		return []any{
+			int64(rand.Int()),
+			randomStr(),
+			randomStr(),
+			randomStr(),
+			randomStr(),
+		}
+	}
+
+	sendOnce := func() {
+		batchBuilder, sender, err := conn.PrepareBatchBuilderAndSender(context.Background(), "INSERT INTO example")
+		require.NoError(t, err)
+
+		for i := 0; i < 1000; i++ {
+			err := batchBuilder.Append(buildRandomRow()...)
+			require.NoError(t, err)
+		}
+
+		buffer := bf.GetBuffer()
+		defer bf.PutBuffer(buffer)
+
+		buffer, err = batchBuilder.Build(buffer)
+		require.NoError(t, err)
+
+		err = sender.Send(context.Background(), buffer)
+		require.NoError(t, err)
+	}
+
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			for i := 0; i < 100; i++ {
+				sendOnce()
+			}
+		}()
+	}
+
+	// wg.Wait()
+}
+
+func randomStr() string {
+	s := []string{"a", "arfghqarhfp;a", "ahfidia;dhfaip", "ghilustghhuhr", "fdhgias", "arhfdpahjrfphsa"}
+	i := rand.Int() % len(s)
+	return s[i]
+}
 
 // func init() {
 // 	if err := os.Setenv("CLICKHOUSE_USE_DOCKER", "false"); err != nil {
